dde6ba9b828a172831f4ba42a1d933441725674a
Kohki YAMAGIWA <tamanobi@gmail.com>
2015-06-24 16:02:13 +0900

    ページタイトルの括弧を無視する

--- extractRuby.py
+++ extractRuby.py
@@ -38,56 +38,66 @@ class FuriganaExtractor:
         title_match = self.title_prog.match(line)
         if title_match:
           self.title_found = True
-          self.page_title = title_match.group('title')
+          # かっこ削除
+          kakko_regex = u'[ 　(（]+([^)）]*)[)）]+'
+          kakko_match = re.search(kakko_regex, title_match.group('title'))
+          #print '*** '+title_match.group('title')
+          if kakko_match:
+            self.page_title = re.sub(kakko_regex, '', title_match.group('title'), 2)
+          else:
+            self.page_title = title_match.group('title')
           kana_match = self.kana_prog.match(self.page_title)
-          #print '******'+self.page_title
           if kana_match:
             # タイトルが,かなだけなら
             self.title_found = False
             noun = kana_match.group(0)
             furigana = noun
             return (self.page_title, noun, furigana)
-
-        if self.title_found:
-          section_match = self.section_prog.match(line)
-          redirect_match = self.redirect_prog.match(line)
-          category_match = self.category_prog.match(line)
-          ruby_match = self.ruby_prog.match(line)
-          if section_match:
-            self.title_found = False
-          elif redirect_match:
-            pass
-          elif category_match:
-            pass
-          elif ruby_match:
-            # ルビを見つけたら
-            self.title_found = False
-            noun = ruby_match.group('noun')
-            furigana = ruby_match.group('ruby')
-            noun = re.sub(u'\[[/]*(tpl)\]', '', noun, 2)
-            furigana = re.sub(u'\[[/]*(tpl)\]', '', furigana, 2)
-            furigana = re.sub(u'[0-9]+[年][0-9]+[月][0-9]+[日]', '', furigana)
-            furigana = re.sub(u'[^ァ-ヾｦ-ﾟぁ-ゟ]*$', '', furigana, 2)
-            ichiran = noun.rfind(u'一覧')
-            # ゴミ除去
-            if re.match('^[a-zA-Z|\- ]+$',furigana):
+        else:
+          if self.title_found:
+            section_match = self.section_prog.match(line)
+            redirect_match = self.redirect_prog.match(line)
+            category_match = self.category_prog.match(line)
+            ruby_match = self.ruby_prog.match(line)
+            if section_match:
+              self.title_found = False
+            elif redirect_match:
               pass
-            elif re.match(u'^[0-9]+[月][0-9]+[日]$',noun):
+            elif category_match:
               pass
-            elif len(furigana) < 2 or len(noun) < 2:
-              pass
-            elif re.match(u'[^ァ-ヾｦ-ﾟぁ-ゟ－ 　]+',furigana):
-              pass
-            elif ichiran > -1:
-              # 一覧を含む文字列
-              pass
-            else:
-              return (self.page_title, noun, furigana)
+            elif ruby_match:
+              # ルビを見つけたら
+              self.title_found = False
+              noun = ruby_match.group('noun')
+              furigana = ruby_match.group('ruby')
+              noun = re.sub(u'\[[/]*(tpl)\]', '', noun, 2)
+              furigana = re.sub(u'\[[/]*(tpl)\]', '', furigana, 2)
+              furigana = re.sub(u'[0-9]+[年][0-9]+[月][0-9]+[日]', '', furigana)
+              furigana = re.sub(u'[^ァ-ヾｦ-ﾟぁ-ゟ]*$', '', furigana, 2)
+              ichiran = noun.rfind(u'一覧')
+              mataha = furigana.find(u'または')
+              # ゴミ除去
+              if re.match('^[a-zA-Z|\- ]+$',furigana):
+                pass
+              elif re.match(u'^[0-9]+[月][0-9]+[日]$',noun):
+                pass
+              elif len(furigana) < 2 or len(noun) < 2:
+                pass
+              elif re.match(u'[^ァ-ヾｦ-ﾟぁ-ゟ－ 　]+',furigana):
+                pass
+              elif ichiran > -1:
+                # 一覧を含む文字列
+                pass
+              elif mataha > -1:
+                # または
+                pass
+              else:
+                return (self.page_title, noun, furigana)
   def Complete(self):
     return self.isComplete
 
 if __name__ == '__main__':
-  ext = FuriganaExtractor('jawiki-latest-pages-articles.xml-001.txt')
+  ext = FuriganaExtractor('jawiki-latest-pages-articles.xml-002.txt')
   while ext.Complete() is False:
     pair = ext.nextPair()
     if pair:
